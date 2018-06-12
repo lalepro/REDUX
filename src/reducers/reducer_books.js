1c5a0632925b28b5fa0022d51d49dea823b934f3
Laura Prochaska <lauraleeprochaska@gmail.com>
2018-06-12 12:22:06 -0500

    working with REDUX

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function(){
+    return [
+        { title:  'JavaScript: The best' },
+        { title: 'Heard it outloud' },
+        { title: 'The Dark Tower' },
+        { title: 'The Sisterhood' }
+    ]
+}
